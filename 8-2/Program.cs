bd25c53b1b088b25b029f1aadab444e5560882b5
Fender-83 <fenderpugach@gmail.com>
2023-02-25 12:43:27 +0700

    8.2

--- 8-2/Program.cs
+++ 8-2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,45 @@ namespace _8_2
 {
     internal class Program
     {
+        public static double GetSize(DirectoryInfo directory)
+        {
+            double summByte = 0;
+            try
+            {
+                if (directory.Exists)
+                {
+                    foreach (var file in directory.GetFiles())
+                    {
+                        summByte += file.Length;
+                    }
+                    foreach (var folder in directory.GetDirectories())
+                    {
+                        summByte += GetSize(folder);
+                    }
+                    return summByte;
+                }
+                else
+                {
+                    Console.WriteLine("По данному пути папки не существует");
+                    return summByte;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return summByte;
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите путь ");
+            string path = Console.ReadLine();
+            var folder = new DirectoryInfo(path);
+            double result = GetSize(folder);
+            if (folder.Exists)
+            {
+                Console.WriteLine("Размер папки на диске составляет " + result + " байт");
+            }
         }
     }
 }
