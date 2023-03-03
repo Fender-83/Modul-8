8745c54298e79fcd5142098b1a9fa818b303ae0b
Name <fenderpugach@gmail.com>
2023-03-03 17:18:20 +0700

    Обновление

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -12,21 +12,30 @@ namespace FinalTask
     class Program
     {
         static void Main(string[] args)
+
         {
+            string path = @"C:\\Users\Fender\Desktop\Students";
+            
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+            dirInfo.CreateSubdirectory(path);
+
             BinaryFormatter formatter = new BinaryFormatter();
             Student[] students;
 
-            using (var fs = new FileStream("Students.dat", FileMode.OpenOrCreate))
+            using (var fs = new FileStream(@"C:\\Students.dat", FileMode.OpenOrCreate))
             {
                 students = (Student[])formatter.Deserialize(fs);
             }
-            Directory.CreateDirectory($"C:\\Students");
-          
+            Console.WriteLine("Папка 'Students' создана на 'Рабочем столе'");
             foreach (var student in students)
             {
-                var path = $"C:\\Students\\{student.Group}.txt";
+                var path2 = $"C:\\Users\\Fender\\Desktop\\Students\\{student.Group}.txt";
 
-                using (StreamWriter sw = File.AppendText(path))
+                using (StreamWriter sw = File.AppendText(path2))
                 {
                     sw.WriteLine($"{student.Name}, {student.DateOfBirth}");
                 }
