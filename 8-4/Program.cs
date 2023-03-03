2ed69bdec9edbb13a91df0688755b56805f31520
Name <fenderpugach@gmail.com>
2023-03-03 15:15:55 +0700

    FinalTask

--- 8-4/Program.cs
+++ 8-4/Program.cs
@@ -4,25 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
 
 namespace FinalTask
 {
+    [Serializable]
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Student(string name, string group, DateTime dateOfBirth)
+        {
+            Name = name;
+            Group = group;
+            DateOfBirth = dateOfBirth;
+        }
+    }
+
     internal class Program
     {
-        [Serializable]
-        class Students
-        {
-            public string Name { get; set; }
-            public string Group { get; set; }
-            public DateTime DateOfBirth { get; set; }
-            public Students(string name, string group, DateTime dateOfBirth)
-            {
-                Name = name;
-                Group = group;
-                DateOfBirth = dateOfBirth;
-            }
-        }
-
+       
         static void Main(string[] args)
         {
             string path = @"C:\\Users\";
@@ -49,7 +52,19 @@ namespace FinalTask
                 string Name = reader.ReadString();
                 string Group = reader.ReadString();
                 string DateOfBirth = reader.ReadString();
+
             }
+            SoapFormatter formatter = new SoapFormatter();
+         
+            using (var fileStream = new FileStream(@"C:\Students.dat", FileMode.OpenOrCreate))
+            {
+                var student = (Student)formatter.Deserialize(fileStream);
+                Console.WriteLine("Объект десериализован");
+                Console.WriteLine($"Имя: {student.Name} --- Возраст: {student.DateOfBirth}");
+            }
+            Console.ReadLine();
+
+
         }
     }
 }
---
+++ FinalTask/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FinalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            Student[] students;
+
+            using (var fs = new FileStream("Students.dat", FileMode.OpenOrCreate))
+            {
+                students = (Student[])formatter.Deserialize(fs);
+            }
+            Directory.CreateDirectory($"C:\\Students");
+          
+            foreach (var student in students)
+            {
+                var path = $"C:\\Students\\{student.Group}.txt";
+
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine($"{student.Name}, {student.DateOfBirth}");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
